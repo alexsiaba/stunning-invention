3b7be7117c95f8359bc278590aa92e9c573c03ed
Alex Siaba <alex.siaba@gmail.com>
2022-06-13 00:27:56 -0400

    compile menus from nodes

--- components/Header/menu.js
+++ components/Header/menu.js
@@ -1,3 +1,57 @@
+class MenuItem {
+  constructor(title, children) {
+    this.title = title
+    this.href = encodeURI(title).toLowerCase()
+    this.id = this.href
+    this.children = children
+  }
+}
+
+class Node {
+  constructor(type, title, parent) {
+    this.type = type
+    this.title = title
+    this.parent = parent
+  }
+}
+
+const pages = [
+  new Node("basic", "index", null),
+  new Node("basic", "About Us", "index"),
+  new Node("basic", "FAQs", "About Us"),
+  new Node("basic", "Testimonials", "About Us"),
+  new Node("basic", "COVID-19", "About Us")
+]
+
+const buildMenu = pages => {
+  const indexPage = pages[0]
+  const root = new MenuItem(indexPage.title, [])
+  const findParent = (root, node) => {
+    if (root.title === node.parent) {
+      return root
+    } else {
+      for (let child of root.children) {
+        const parent = findParent(child, node)
+        if (parent !== null) {
+          return parent
+        }
+      }
+    }
+    return null
+  }
+  pages.slice(1).forEach(page => {
+    const parent = findParent(root, page)
+    parent.children.push(new MenuItem(page.title, []))
+  })
+  return root;
+}
+
+const myMenu = buildMenu(pages)
+
+console.log(myMenu)
+
+
+
 const menu = [
   {
     name: "About",
