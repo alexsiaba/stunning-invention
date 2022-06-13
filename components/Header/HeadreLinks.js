466b935ab87b91b4ca8f71daed7830f566ea075d
Alex Siaba <alex.siaba@gmail.com>
2022-06-13 16:48:22 -0400

    almost there

--- components/Header/HeadreLinks.js
+++ components/Header/HeadreLinks.js
@@ -16,35 +16,58 @@ import menu, { myMenu } from "./menu";
 import styles from "./headerLinksStyle";
 import MenuItem from "@material-ui/core/MenuItem";
 import CustomDropdown1 from "../CustomDropdown/CustomDropdown1";
+import Button from "@material-ui/core/Button";
+import classNames from "classnames";
+import {Popper} from "@material-ui/core";
 
 const useStyles = makeStyles(styles);
 console.log("myMenu", myMenu);
 export default function HeaderLinks(props) {
-  const classes = useStyles();
-  const [headerMenuList] = useState(menu);
+    const classes = useStyles();
+    const [headerMenuList] = useState(menu);
 
     const myMenuToHeaderLinks = (node, level) => {
+        const buttonProps = {
+            className: classes.navLink,
+            color: "transparent"
+        }
+        const caretClasses = classNames({
+            [classes.caret]: true,
+            //[classes.caretActive]: Boolean(anchorEl),
+        })
         switch (level) {
             case 0:
                 return React.createElement(List, {
                     className: classes.list,
-                    children: node.children.map(child => myMenuToHeaderLinks(child, level+1))
+                    children: node.children.map(child => myMenuToHeaderLinks(child, level + 1))
                 })
             case 1:
-                return React.createElement(ListItem, {
-                    className: classes.listItem,
-                    children: node.children.map(child => myMenuToHeaderLinks(child, level+1))
-                })
+                let button = (
+                    <Button {...buttonProps}>
+                        {node.title}
+                        {node.children.length > 0 && <b className={caretClasses}/>}
+                        {node.children.length > 0 && (() => {
+                            const [anchorEl, setAnchorEl] = React.useState(null);
+                            return <Popper
+                                open={Boolean(anchorEl)}
+                                anchorEl={anchorEl}
+                                transition
+                                disablePortal
+                                className={classNames({
+                                    [classes.popperClose]: !anchorEl,
+                                    [classes.popperResponsive]: true,
+                                    [classes.pooperNav]: Boolean(anchorEl) && navDropdown,
+                                })}
+                            >
+                                {node.children.map(child => myMenuToHeaderLinks(child, level + 1))}
+                            </Popper>
+                        })()
+                        }
+
+                    </Button>
+                )
+                return <ListItem className={classes.listItem}>{button}</ListItem>
             case 2:
-                return React.createElement(CustomDropdown1, {
-                    buttonText: node.title,
-                    buttonProps: {
-                        className: classes.navLink,
-                        color: "transparent"
-                    },
-                    children: [] //node.children.map(child => myMenuToHeaderLinks(child, level+1))
-                })
-            case 3:
                 return (
                     <MenuItem key={node.id}>
                         <Link href={node.href}>
@@ -54,5 +77,7 @@ export default function HeaderLinks(props) {
                 )
         }
     }
-  return myMenuToHeaderLinks(myMenu, 0)
+    const compiledMenu = myMenuToHeaderLinks(myMenu, 0)
+    console.log(compiledMenu)
+    return compiledMenu;
 }
