7e347bad3a15218f4edfca8d688826a759dbba9b
Alex Siaba <alex.siaba@gmail.com>
2022-06-18 15:30:59 -0400

    get rid of i18n middleware

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const next = require('next')
-const nextI18NextMiddleware = require('next-i18next/middleware').default
-
-const nextI18next = require('./i18n')
 
 const port = process.env.PORT || 3002
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
@@ -11,8 +8,6 @@ const handle = app.getRequestHandler();
 (async () => {
   await app.prepare()
   const server = express()
-
-  server.use(nextI18NextMiddleware(nextI18next))
 
   server.get('*', (req, res) => handle(req, res))
 
